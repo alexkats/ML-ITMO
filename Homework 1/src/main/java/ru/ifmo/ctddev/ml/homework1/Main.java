c9202bb818a47eef816bc934d316ee622a221124
MaximSlyusarenko <slyuslyu.m@mail.ru>
2016-09-17 19:51:57 +0300

    Lombok added

--- Homework 1/src/main/java/ru/ifmo/ctddev/ml/homework1/Main.java
+++ Homework 1/src/main/java/ru/ifmo/ctddev/ml/homework1/Main.java
@@ -1,5 +1,7 @@
 package ru.ifmo.ctddev.ml.homework1;
 
+import ru.ifmo.ctddev.core.entities.TwoDimensionalPoint;
+
 /**
  * @author Alexey Katsman
  * @since 17.09.16
@@ -8,5 +10,7 @@ package ru.ifmo.ctddev.ml.homework1;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Test 2");
+		TwoDimensionalPoint point = new TwoDimensionalPoint(1.0, 2.0);
+		System.out.println(point.getX());
 	}
 }
---
+++ core/src/main/java/ru/ifmo/ctddev/core/entities/TwoDimensionalPoint.java
@@ -0,0 +1,20 @@
+package ru.ifmo.ctddev.core.entities;
+
+import lombok.Getter;
+
+/**
+ * @author Maxim Slyusarenko
+ * @since 17.09.16
+ */
+@Getter
+public class TwoDimensionalPoint {
+
+    private final double x;
+    private final double y;
+
+    public TwoDimensionalPoint(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+}
