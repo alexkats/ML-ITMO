545f4bde214d0cdec4246a6005c7cefb51691f85
Alexey Katsman <alex2007508@gmail.com>
2016-09-17 14:31:28 +0300

    Initial commit

---
+++ Homework 1/src/main/java/ru/ifmo/ctddev/ml/homework1/Main.java
@@ -0,0 +1,12 @@
+package ru.ifmo.ctddev.ml.homework1;
+
+/**
+ * @author Alexey Katsman
+ * @since 17.09.16
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Test");
+	}
+}
---
+++ Homework 1/src/test/java/ru/ifmo/ctddev/ml/homework1/MainTest.java
@@ -0,0 +1,16 @@
+package ru.ifmo.ctddev.ml.homework1;
+
+import org.junit.Assert;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Alexey Katsman
+ * @since 17.09.16
+ */
+public class MainTest {
+	@org.junit.Test
+	public void main() throws Exception {
+		Assert.assertEquals("Test", "Test");
+	}
+}
