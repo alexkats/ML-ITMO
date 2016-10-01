fc55a6965ad0f1dbc86eae61d8300c5b6ef27e77
Alexey Katsman <alex2007508@gmail.com>
2016-10-01 11:07:51 +0300

    Equals and hashcode added to DataSetEntity

--- Homework 2/src/main/java/ru/ifmo/ctddev/ml/homework2/DataSetEntity.java
+++ Homework 2/src/main/java/ru/ifmo/ctddev/ml/homework2/DataSetEntity.java
@@ -14,4 +14,28 @@ public class DataSetEntity {
     private final int area;
     private final int room;
     private final int price;
+
+    @Override
+    public int hashCode() {
+        int hash = area;
+        hash += 31 * room;
+        hash += 31 * 31 * price;
+
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        DataSetEntity other = (DataSetEntity) obj;
+
+        return area == other.area && room == other.room && price == other.price;
+    }
 }
