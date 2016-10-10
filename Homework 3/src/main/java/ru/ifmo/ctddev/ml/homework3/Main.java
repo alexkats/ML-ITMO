cb91f4a69e6018cf71a74b883d43c9a022cf9bd4
Alexey Katsman <alex2007508@gmail.com>
2016-10-10 15:40:40 +0300

    Homework 3 initialized

---
+++ Homework 3/src/main/java/ru/ifmo/ctddev/ml/homework3/Main.java
@@ -0,0 +1,12 @@
+package ru.ifmo.ctddev.ml.homework3;
+
+/**
+ * @author Alexey Katsman
+ * @since 10.10.16
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
